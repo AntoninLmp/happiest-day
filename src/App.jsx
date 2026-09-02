3a6d44d71045ec4b6791bad355cc142194f93d90
Antonin Lmp <73228586+AntoninLmp@users.noreply.github.com>
2026-09-02 21:21:39 +0200

    feat<Router>: Update redirection url

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from "./components/Home";
 import LoveStory from "./components/LoveStory";
 import Programme from './components/section/Programme';
@@ -7,13 +7,13 @@ import Programme from './components/section/Programme';
 
 export default function App (){
   return (
-    <BrowserRouter basename='happiest-day'>
+    <Router>
         <Routes>
           <Route path="/" element={<Home isLetter={true}/>} />
           <Route path="/home" element={<Home isLetter={false} />} />
           <Route path="/histoire" element={<LoveStory />} />
           <Route path="/programme" element={<Programme />} />
         </Routes>
-    </BrowserRouter>
+    </Router>
   ); 
 }
